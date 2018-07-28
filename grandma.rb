3647606af3b67b2eaff211277c5706c7d0e766b1
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-07-28 12:50:32 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(speaking)
-  if speaking = "I LOVE YOU GRANDMA!"
+  if speaking == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
       elsif speaking.upcase == speaking
         return "NO, NOT SINCE 1938!"
